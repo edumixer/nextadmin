cf10a096a41c93dc862defb763ca0cc2ecc2b02d
edumixer <edu.cena.wf@gmail.com>
2024-02-09 16:39:21 -0300

    feat: create LoginPage page

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,16 @@
+import styles from "@/app/ui/login/login.module.css";
+
 const LoginPage = () => {
   return (
-    <h1>LoginPage</h1>
-  )
-}
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Order Swift Login</h1>
+        <input type="text" placeholder="Usuário" name="username" />
+        <input type="password" placeholder="Senha" name="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
+};
 
 export default LoginPage;
