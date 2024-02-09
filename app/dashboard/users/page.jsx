8c1b6098c29ea41840991c8f1ee927ca704e9426
edumixer <edu.cena.wf@gmail.com>
2024-02-09 11:39:34 -0300

    feat: create page to render table of users and create one of them

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,7 +1,65 @@
+import Pagination from "@/app/ui/dashboard/pagination/pagination";
+import Search from "@/app/ui/dashboard/search/search";
+import styles from "@/app/ui/dashboard/users/users.module.css";
+import Image from "next/image";
+import Link from "next/link";
+
 const UsersPage = () => {
   return (
-    <div>UsersPage</div>
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Buscar por um usuário"/>
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add User</button>
+        </Link>
+      </div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Nome</td>
+            <td>E-mail</td>
+            <td>Created At</td>
+            <td>Função</td>
+            <td>Status</td>
+            <td>Ação</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />
+                John Doe
+              </div>
+            </td>
+            <td>john@gmail.com</td>
+            <td>02.02.2024</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
+                <Link href="/dashboard/users/test">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    Visualizar
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Deletar
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <Pagination />
+    </div>
   );
-}
+};
 
 export default UsersPage;
